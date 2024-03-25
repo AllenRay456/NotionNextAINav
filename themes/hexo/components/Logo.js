0a723446aeb1f8c8135eceb8178a4b3f189b0062
ray <yutianriyue@gmail.com>
2024-03-25 11:51:09 +0800

    chore

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 const Logo = props => {
   return (
-    <Link href='/' passHref legacyBehavior>
+    <Link href='https://openai.xnewstar.com/' passHref legacyBehavior>
       <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
-        <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'> {siteConfig('TITLE') }</div>
+        <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-red-300 menu-link transform duration-200'> {siteConfig('TITLE') }</div>
       </div>
     </Link>
   )
